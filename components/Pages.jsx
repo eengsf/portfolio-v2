7979e06cd5d89ba12667534b1f0cef9c397a688e
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-04-29 07:37:43 +0700

    update1

--- components/Pages.jsx
+++ components/Pages.jsx
@@ -11,7 +11,7 @@ const Pages = () => {
       <div className="2md:hidden block">
         <TopNavbar />
       </div>
-      <div className="flex 2md:flex-row flex-col max-w-6xl 2md:h-screen h-full mx-auto 2md:py-12 pb-5 pt-28 2lg:px-0 2md:px-10 px-5 gap-12">
+      <div className="flex 2md:flex-row flex-col max-w-6xl h-full mx-auto 2md:py-12 pb-5 pt-28 2lg:px-0 2md:px-10 px-5 gap-12">
         <aside className="w-60 h-full 2md:flex hidden">
           <Aside />
         </aside>
