ba6062a1833ffc4082be49474e6e8c0a2bdbd5af
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-05-14 15:27:17 +0700

    delete topic project

--- components/MainProjectWeb.jsx
+++ components/MainProjectWeb.jsx
@@ -1,40 +1,53 @@
 import MainProjectWebDetail from "./MainProjectWebDetail";
 
 const obj = {
+  dashboardKit: {
+    img: "dashboard-kit.png",
+    name: "Dashboard Kit",
+    desc: "This application is a project that utilizes an API to display a list of customer tickets. It includes features to search for customers, pagination, filtering, and sorting by name.",
+    tech: ["Next.js", "Tailwindcss", "Redux Toolkit"],
+    linkDemo: "https://dashboard-kit-henna.vercel.app/",
+    linkCode: "https://github.com/eengsf/dashboard-kit",
+  },
   articleListApp: {
     img: "article-list-app.png",
     name: "Article List App",
     desc: "This application is a project that utilizes an API to display articles. It includes a feature to search for articles from several countries, specific categories, and other general topics.",
     tech: ["Next.js", "Tailwindcss"],
-    link: "https://article-list-one.vercel.app/",
+    linkDemo: "https://article-list-one.vercel.app/",
+    linkCode: "https://github.com/eengsf/article-list",
   },
   animeListApp: {
     img: "anime-list-app.png",
     name: "Anime List App",
     desc: "This application is a project that utilizes an API to display a list of popular anime. It includes a feature to search for anime titles and a pagination system.",
     tech: ["Next.js", "Tailwindcss"],
-    link: "https://anime-list-app-seven.vercel.app/",
+    linkDemo: "https://anime-list-app-seven.vercel.app/",
+    linkCode: "https://github.com/eengsf/anime-list-app",
   },
   topicCRUD: {
     img: "topic-crud.png",
     name: "Topic CRUD",
     desc: `This application is a simple project that features CRUD (Create, Read, Update, Delete) operations using Next.js, Tailwind CSS, and MongoDB as its database.`,
     tech: ["Next.js", "Tailwindcss", "Mongodb"],
-    link: "https://cobafullstack.vercel.app/",
+    linkDemo: "https://cobafullstack.vercel.app/",
+    linkCode: "https://github.com/eengsf/crud-topic",
   },
   catatanBelanja: {
     img: "catatan-belanja.png",
     name: "Shopping List",
     desc: "This application is a simple project to display saved shopping lists. It includes a feature to filter existing shopping lists.",
     tech: ["React.js", "Vite.js"],
-    link: "https://catatan-belanja-zeta.vercel.app/",
+    linkDemo: "https://catatan-belanja-zeta.vercel.app/",
+    linkCode: "https://github.com/eengsf/catatan-belanja",
   },
   responsifWeb: {
     img: "responsif-web.png",
     name: "Responsif Web",
     desc: `An interactive and responsive web project with several supported animation views, built with modern Next.js technology.`,
     tech: ["Next.js", "Tailwindcss", "Redux Toolkit", "Framer Motion"],
-    link: "https://asc-sf.vercel.app/",
+    linkDemo: "https://asc-sf.vercel.app/",
+    linkCode: "https://github.com/eengsf/asc-sf",
   },
   portfolio: {
     img: "portfolio-v2.png",
@@ -47,54 +60,85 @@ const obj = {
       "Framer Motion",
       "Mongodb",
     ],
-    link: "https://portfolio-v2-rho-murex.vercel.app/",
+    linkDemo: "https://portfolio-v2-rho-murex.vercel.app/",
+    linkCode: "https://github.com/eengsf/portfolio-v2",
+  },
+  onlineShop: {
+    img: "onlineshopeeng.png",
+    name: "Online Shop",
+    desc: "A simple online shop application with features for category filtering, sorting, and searching for products, as well as adding items to a shopping cart that is saved to each user's account.",
+    tech: ["Laravel", "React.js", "Tailwindcss"],
+    linkDemo: "https://online-shop.vercel.app/",
+    linkCode: "https://github.com/eengsf/eeng-shop",
   },
 };
 
 const MainProjectWeb = () => {
   return (
-    <div className="grid md:grid-cols-2 grid-cols-1 w-full gap-0 py-8">
+    <div className="grid w-full grid-cols-1 gap-0 py-8 md:grid-cols-2">
+      <MainProjectWebDetail
+        img={obj.dashboardKit.img}
+        name={obj.dashboardKit.name}
+        desc={obj.dashboardKit.desc}
+        tech={obj.dashboardKit.tech}
+        linkCode={obj.dashboardKit.linkCode}
+        linkDemo={obj.dashboardKit.linkDemo}
+      />
       <MainProjectWebDetail
         img={obj.portfolio.img}
         name={obj.portfolio.name}
         desc={obj.portfolio.desc}
         tech={obj.portfolio.tech}
-        link={obj.portfolio.link}
+        linkCode={obj.portfolio.linkCode}
+        linkDemo={obj.portfolio.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.onlineShop.img}
+        name={obj.onlineShop.name}
+        desc={obj.onlineShop.desc}
+        tech={obj.onlineShop.tech}
+        linkCode={obj.onlineShop.linkCode}
+        linkDemo={obj.onlineShop.linkDemo}
       />
       <MainProjectWebDetail
         img={obj.responsifWeb.img}
         name={obj.responsifWeb.name}
         desc={obj.responsifWeb.desc}
         tech={obj.responsifWeb.tech}
-        link={obj.responsifWeb.link}
+        linkCode={obj.responsifWeb.linkCode}
+        linkDemo={obj.responsifWeb.linkDemo}
       />
       <MainProjectWebDetail
         img={obj.articleListApp.img}
         name={obj.articleListApp.name}
         desc={obj.articleListApp.desc}
         tech={obj.articleListApp.tech}
-        link={obj.articleListApp.link}
+        linkCode={obj.articleListApp.linkCode}
+        linkDemo={obj.articleListApp.linkDemo}
       />
       <MainProjectWebDetail
         img={obj.animeListApp.img}
         name={obj.animeListApp.name}
         desc={obj.animeListApp.desc}
         tech={obj.animeListApp.tech}
-        link={obj.animeListApp.link}
+        linkCode={obj.animeListApp.linkCode}
+        linkDemo={obj.animeListApp.linkDemo}
       />
-      <MainProjectWebDetail
+      {/* <MainProjectWebDetail
         img={obj.topicCRUD.img}
         name={obj.topicCRUD.name}
         desc={obj.topicCRUD.desc}
         tech={obj.topicCRUD.tech}
-        link={obj.topicCRUD.link}
-      />
+        linkCode={obj.topicCRUD.linkCode}
+        linkDemo={obj.topicCRUD.linkDemo}
+      /> */}
       <MainProjectWebDetail
         img={obj.catatanBelanja.img}
         name={obj.catatanBelanja.name}
         desc={obj.catatanBelanja.desc}
         tech={obj.catatanBelanja.tech}
-        link={obj.catatanBelanja.link}
+        linkCode={obj.catatanBelanja.linkCode}
+        linkDemo={obj.catatanBelanja.linkDemo}
       />
     </div>
   );
--- components/MainProjectWebDetail.jsx
+++ components/MainProjectWebDetail.jsx
@@ -4,7 +4,14 @@ import Logo from "./Logo";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 
-const MainProjectWebDetail = ({ img, name, desc, tech, link }) => {
+const MainProjectWebDetail = ({
+  img,
+  name,
+  desc,
+  tech,
+  linkCode,
+  linkDemo,
+}) => {
   const { theme } = useSelector((state) => state.counterSlice);
   return (
     <motion.div
@@ -13,25 +20,31 @@ const MainProjectWebDetail = ({ img, name, desc, tech, link }) => {
       transition={{ duration: 0.7 }}
       className="p-2">
       <div className="w-full md:h-[450px] sm:h-[420px] h-full rounded-lg border border-slate-300 group/project transform transition-all duration-300 hover:scale-[102%] pb-4">
-        <Link href={link}>
-          <div className="w-full md:h-[55%] sm:h-[65%] h-[50%] relative">
-            <div className="absolute top-0 left-0 flex gap-2 justify-center items-center w-full h-full font-semibold text-white bg-black/70 rounded-t-lg text-lg opacity-0 transition-all duration-300 group-hover/project:opacity-100">
-              <h1>View Project</h1>
-              <Logo name={"arrowright"} size={24} />
-            </div>
-            <Image
-              src={`/${img}`}
-              alt="image"
-              width={500}
-              height={500}
-              className="object-cover object-top rounded-t-lg w-full h-full"
-            />
+        <div className="w-full md:h-[55%] sm:h-[65%] h-[50%] relative">
+          <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full gap-5 text-white transition-all duration-300 rounded-t-lg opacity-0 bg-black/70 group-hover/project:opacity-100">
+            <Link
+              href={linkCode}
+              className="px-4 py-2 rounded-full bg-slate-400 hover:scale-110">
+              <h1 className="text-sm font-bold">Code</h1>
+            </Link>
+            <Link
+              href={linkDemo}
+              className="px-4 py-2 rounded-full bg-slate-400 hover:scale-110">
+              <h1 className="text-sm font-bold">Demo</h1>
+            </Link>
           </div>
-        </Link>
+          <Image
+            src={`/${img}`}
+            alt="image"
+            width={500}
+            height={500}
+            className="object-cover object-top w-full h-full rounded-t-lg"
+          />
+        </div>
         <div className="flex flex-col gap-2 justify-between w-full md:h-[45%] sm:h-[35%] h-[50%] p-5">
           <div className="flex flex-col gap-1">
             <Link
-              href={link}
+              href={linkDemo}
               className={`font-semibold text-xl ${
                 theme
                   ? "group-hover/project:text-blue-500"
@@ -39,15 +52,15 @@ const MainProjectWebDetail = ({ img, name, desc, tech, link }) => {
               } `}>
               {name}
             </Link>
-            <p className="text-justify text-sm ">{desc}</p>
+            <p className="text-sm text-justify ">{desc}</p>
           </div>
-          <div className="flex gap-3 relative">
+          <div className="relative flex gap-3">
             {tech.map((item, index) => (
               <div
                 key={index}
-                className="group/tech cursor-pointer overflow-x-hidden">
+                className="overflow-x-hidden cursor-pointer group/tech">
                 <Logo name={item} size={24} />
-                <div className="absolute -z-10 transition-all duration-300 opacity-0 group-hover/tech:opacity-100 group-hover/tech:z-0 top-0 right-0">
+                <div className="absolute top-0 right-0 transition-all duration-300 opacity-0 -z-10 group-hover/tech:opacity-100 group-hover/tech:z-0">
                   <p>{item}</p>
                 </div>
               </div>
